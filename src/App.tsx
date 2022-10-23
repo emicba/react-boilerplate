5ed3082eb556545298aece975dee8e5b7d7b36a1
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2022-10-23 12:57:32 -0300

    :tada:

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-purple-400">hello world :^)</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, nam.</p>
+    </div>
+  );
+}
+
+export default App;
